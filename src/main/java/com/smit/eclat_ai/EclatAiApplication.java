4e617ed6a6dc2379a35c2982d969e4da9e6fc10c
Smit Roy <smitroy22@gmail.com>
2026-08-30 22:49:23 +0530

    Project Initiation; Database Connection; Basic Dependency Management and Library Installation

---
+++ src/main/java/com/smit/eclat_ai/EclatAiApplication.java
@@ -0,0 +1,13 @@
+package com.smit.eclat_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclatAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclatAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smit/eclat_ai/EclatAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smit.eclat_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclatAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
